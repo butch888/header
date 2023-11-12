2dc8b64d858b7e13762d312bc2c9bb385f1736a4
Vlad <90765364+butch888@users.noreply.github.com>
2023-11-12 15:58:57 +0300

    added section__collection

--- main.js
+++ main.js
@@ -1,8 +1,29 @@
 'use strict';
 
-let data = localStorage.getItem('key')
+let burger = document.querySelector('.burger');
 
-console.log(data)
+let test = document.querySelector('.test');
+
+let body = document.querySelector('body');
+
+
+burger.addEventListener('click', function(e) {
+    e.stopPropagation();
+    test.classList.toggle('active');
+    body.classList.toggle('active-body');
+})
+
+body.addEventListener('click', function() {
+    test.classList.remove('active');
+    body.classList.remove('active-body');
+})
+
+
+
+
+
+
+
 
 
 
